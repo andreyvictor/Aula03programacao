218b2462f75ef41a1fcb433e2f4957fde6027fb2
04052973 <andreyvictorfotografias@outlook.com>
2019-09-10 09:26:09 -0300

    hello!

--- src/Aula02/LetraI.java
+++ src/Aula02/LetraI.java
@@ -6,8 +6,7 @@ public class LetraI {
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
         double altura;
-        double sexo;
-        double pesoIdeal;
+        double sexo, pesoIdeal;
         System.out.println("Bem vindo, para calcular seu peso ideal vamos precisar se algumas informações!\n ");
         System.out.println("Primeiramente, informe seu sexo. Digite 1 ou 2 para  masculino e feminino respectivamente: ");
         sexo=scanner.nextDouble();
